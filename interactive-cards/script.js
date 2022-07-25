64814493e38a24be4235d5e87ae57231701fe32e
Dev P Ishaili <devpishaili@gmail.com>
2022-07-25 13:48:10 +0100

    Messaging Feature to Interactive Card.

--- interactive-cards/script.js
+++ interactive-cards/script.js
@@ -26,6 +26,62 @@ customElements.define(
       const shadowRoot = this.attachShadow({ mode: "open" }).appendChild(
         myprofile.cloneNode(true)
       );
+
+      // Grabbing the message button and the div wrapping the profile for later use.
+      let msgbtn = this.shadowRoot.querySelector(".messagebtn"),
+        profileEl = this.shadowRoot.querySelector(".profile-wrapper"),
+        zname = this.getElementsByTagName("span")[0].innerHTML;
+
+      // Adding the event listener
+      msgbtn.addEventListener("click", function (e) {
+        // creating all the elements we'll need to build our form
+        let formEl = document.createElement("form"),
+          subjectEl = document.createElement("input"),
+          subjectlabel = document.createElement("label"),
+          contentEl = document.createElement("textarea"),
+          contentlabel = document.createElement("label"),
+          submitEl = document.createElement("input"),
+          closebtn = document.createElement("button");
+
+        //   Setting up the form element. The action just goes to a page
+        formEl.setAttribute("method", "post");
+        formEl.setAttribute(
+          "action",
+          "https://johnrhea.com/undead-form-practice.php"
+        );
+        formEl.classList.add("hello");
+
+        // Setting up a close button so we can close the message if we get shy
+        closebtn.innerHTML = "x";
+        closebtn.addEventListener("click", function () {
+          formEl.remove();
+        });
+
+        // Setting up form fields and labels
+        subjectEl.setAttribute("type", "text");
+        subjectEl.setAttribute("name", "subj");
+        subjectlabel.setAttribute("for", "subj");
+        subjectlabel.innerHTML = "Subject:";
+
+        contentEl.innerHTML = "Hi " + zname + ",\nI like your braaains...";
+        contentEl.setAttribute("name", "cntnt");
+        contentlabel.setAttribute("for", "cntnt");
+        contentlabel.innerHTML = "Message:";
+
+        submitEl.setAttribute("type", "submit");
+        submitEl.setAttribute("value", "Send Message");
+
+        // Putting all the elments in the Form
+        formEl.appendChild(closebtn);
+        formEl.appendChild(subjectlabel);
+        formEl.appendChild(subjectEl);
+        formEl.appendChild(contentlabel);
+        formEl.appendChild(contentEl);
+        formEl.appendChild(submitEl);
+
+        // Putting the form on the page
+        profileEl.appendChild(formEl);
+      });
     }
   }
 );
